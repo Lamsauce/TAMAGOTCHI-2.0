cd5c6518b3904ed4d114adc9ca6178a349e60c77
Lamsauce <lilylam743@gmail.com>
2021-06-10 22:31:18 -0700

    enter key for name works

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,19 @@
 
 
 function nameDeclare(){
+    $("#confirmName").on("click", function(event){
     $("#name").replaceWith("<p>" + $("#userInputName").val() + "</p>"); 
     $(".nameInput").hide();
+    })
+    
+    $('#userInputName').bind("enterKey",function(e){
+    });
+
+    $('#userInputName').keyup(function(e){
+    if(e.keyCode == 13) {
+        $(this).trigger("enterKey");
+        $("#name").replaceWith("<p>" + $("#userInputName").val() + "</p>"); 
+        $(".nameInput").hide();
+    }
+  })
 }; 
