daa0dc62a2f23e9c04ec9ab927031c004d177430
Lamsauce <lilylam743@gmail.com>
2021-06-14 20:58:59 -0700

    pet stages added to html

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,12 @@
+/* Temporary sprite scripts */
+$("#charmander").hide();
+$("#charmeleon").hide();
+$("#charizard").hide();
+$("#missingNo").hide();
+
+
 
 /* replaces name ID, allow user to change name */
-
-
-
 function nameDeclare(){
     $("#confirmName").on("click", function(event){
     $("#name").replaceWith("<p>" + $("#userInputName").val() + "</p>"); 
@@ -16,7 +20,29 @@ function nameDeclare(){
     if(e.keyCode == 13) {
         $(this).trigger("enterKey");
         $("#name").replaceWith("<p>" + $("#userInputName").val() + "</p>"); 
+        $("#userInputName").css({"font-size": "40px;", "color": "red"});
         $(".nameInput").hide();
     }
   })
 }; 
+
+let hunger = 10;
+let energy = 10;
+let entertainment = 10;
+let time = 60;
+
+//Life Status
+  //Green = 7+ Value
+  //Yellow = 4-6
+  //Red = 1-3
+
+//Add interaction animation like eating and entertainment
+  //Hungy --> feed --> Give food animation
+
+//Evolve tamagotchi
+
+//Age / Time system
+
+
+
+//ICEBOX: Money system with bonus and buying items
